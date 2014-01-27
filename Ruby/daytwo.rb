7068afbaed4ea9c0690d7f334c48bf3a36ff53d8
Jordan Robinson <hypahnova@gmail.com>
2014-01-27 20:04:12 +0000

    Some exercises from day two, still needs some more work.

---
+++ Ruby/daytwo.rb
@@ -0,0 +1,27 @@
+class DayTwo
+
+  def run
+    array_each
+  end
+
+  def array_each
+    a = []
+    for i in 0..15
+      a << i
+    end
+
+    a.each do |i|
+      if a.rindex(i) % 4 == 0
+        puts a[a.rindex(i)..a.rindex(i)+3].join(" ")
+      end
+    end
+
+  end
+
+
+
+
+
+
+  self
+end.new.run
