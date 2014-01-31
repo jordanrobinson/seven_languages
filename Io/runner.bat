3c2d05266afcdf68c731dab6839499998ec28a97
Jordan Robinson <hypahnova@gmail.com>
2014-01-31 22:05:44 +0000

    Sets up Io exercises.

---
+++ Io/runner.bat
@@ -0,0 +1,6 @@
+D:\0\iobin-win32-current\IoLanguage\bin\io_static.exe D:\0\Dropbox\Work\seven_languages\Io\dayone.io
+PAUSE
+D:\0\iobin-win32-current\IoLanguage\bin\io_static.exe D:\0\Dropbox\Work\seven_languages\Io\daytwo.io
+PAUSE
+D:\0\iobin-win32-current\IoLanguage\bin\io_static.exe D:\0\Dropbox\Work\seven_languages\Io\daythree.io
+PAUSE
