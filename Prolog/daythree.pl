b0b0bb9e3998f2bd0f9e5f28df1d9e78ffa5078c
Jordan Robinson <hypahnova@gmail.com>
2014-02-21 00:06:49 +0000

    Starts day three of Prolog.

---
+++ Prolog/daythree.pl
@@ -0,0 +1,5 @@
+%exercise one - modify sudoku solver to work on six by six puzzles
+
+%exercise two - make the sudoku solver print prettier solutions
+
+%exercise three - eight queens problem (this one may be a little hard)
