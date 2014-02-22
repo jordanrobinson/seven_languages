f5fccb807b01d1297b6ab1d8d6dbd32d6da220df
Jordan Robinson <hypahnova@gmail.com>
2014-02-22 23:45:29 +0000

    Day three of prolog. Sort of.

--- Prolog/daythree.pl
+++ Prolog/daythree.pl
@@ -3,3 +3,5 @@
 %exercise two - make the sudoku solver print prettier solutions
 
 %exercise three - eight queens problem (this one may be a little hard)
+
+%The prolog chapter really drags it's heels so I'll probably come back to this at some point and actually do the exercises.
