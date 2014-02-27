829d7f03c9b2e6c67679b6ef535fd711c35b2458
Jordan Robinson <hypahnova@gmail.com>
2014-02-27 19:42:45 +0000

    More day one for scala.
    Starts on the board for noughts and crosses, adds some more info on tuples.

---
+++ Scala/dayone/board.scala
@@ -0,0 +1,9 @@
+
+/* exercise one - write a game that will take a tic-tac-toe board with X, O,
+	 and blank characters and detect the winner or whether there is a tie or 
+	 no winner yet. Use classes where appropriate. */
+class Board(val cells: List[Char]) {
+	def prettyPrint() {
+		println(cells)
+	}
+}
