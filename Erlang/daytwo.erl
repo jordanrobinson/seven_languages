b120dd204ff0bf0e275ae1377c08e012c10ff332
Jordan Robinson <hypahnova@gmail.com>
2014-03-30 17:52:29 +0100

    Finishes day two of Erlang
    Adds the list comprehension exercise, which is something that Erlang seems to be pretty good at.

--- Erlang/daytwo.erl
+++ Erlang/daytwo.erl
@@ -3,7 +3,8 @@
 -export([process_list/2]).
 -export([quantity_comprehension/1]).
 
-run() -> io:fwrite(process_list(y, [{x,"fire"},{y,"fira"},{z,"firaga"}])).
+%run() -> io:fwrite(process_list(y, [{x,"fire"},{y,"fira"},{z,"firaga"}])).
+run() -> erlang:display(quantity_comprehension([{pen, 1, 3}, {pencil, 2, 5}, {robopencil, 4, 13}])).
 
 % exercise one - write a function that accepts a list and keyword, and returns the associated value for the keyword
 process_list(Word, List) ->
@@ -16,4 +17,5 @@ process_list(Word, List) ->
 
 % exercise two - write a list comprehension that builds a list of items of the form
 % [{item,quantity,price},...] where total price is quantity times price
-quantity_comprehension(List) -> List.
+quantity_comprehension(List) ->
+  [{Item, (Quantity * Price)} || {Item, Quantity, Price} <- List]. %This is surprisingly elegant
