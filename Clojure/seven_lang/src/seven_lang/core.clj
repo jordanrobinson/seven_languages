75c936d7fd3de55cd3bc50ff4fbe97eb95cae654
Jordan Robinson <hypahnova@gmail.com>
2014-04-11 12:16:52 +0100

    Simple hello world for day one of Clojure

--- Clojure/seven_lang/src/seven_lang/core.clj
+++ Clojure/seven_lang/src/seven_lang/core.clj
@@ -1,7 +1,8 @@
 (ns seven-lang.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn hello
+  "simple hello world"
+  []
+  (println "Hello there clojure"))
 
+(hello)
