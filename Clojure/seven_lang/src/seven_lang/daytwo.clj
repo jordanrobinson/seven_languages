8f6e3be666537407bda5644720a4020963f3968e
Jordan Robinson <hypahnova@gmail.com>
2014-04-18 21:45:13 +0100

    Some more work on day 2 of Clojure
    Finishes the first exercise, and adds a good amount of work towards the second.

--- Clojure/seven_lang/src/seven_lang/daytwo.clj
+++ Clojure/seven_lang/src/seven_lang/daytwo.clj
@@ -2,5 +2,23 @@
 
 ;exercise one - implement an unless with an else condition using macros
 
+(defmacro unless [test body] ;this is the original, similar to the book example
+  (list 'if (list 'not test) body)
+  )
+(defmacro unlesselse [test body otherwise] ;this is the original, similar to the book example
+  (list 'if (list 'not test) body otherwise) ;I could be missing something here, this seems deceptively simple
+  )
+
 ;exercise two - write a type using defrecord that implements a protocol
 
+(defprotocol Test
+  (pass-test [this])
+  (fail-test [this])
+  )
+
+(defrecord Tester [name]
+  Test
+  (pass-test
+    [this] (println "Success!")
+    )
+  )
